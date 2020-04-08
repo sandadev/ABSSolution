dae7bb861c490e919a7704a750553e9faac6703f
sandadev <santhoshkumartalari@outlook.com>
2020-04-08 21:30:02 +0530

    created new csharp api solution

---
+++ ABSProject/ABS_API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ABS_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ABSProject/ABS_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ABS_API.WebApiApplication" Language="C#" %>
